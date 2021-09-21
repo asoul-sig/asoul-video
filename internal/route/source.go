07077fd9f39405c31bba1bc1b553c4c5adc89a2a
E99p1ant <i@github.red>
2021-09-21 14:48:52 +0800

    update(source): support mutli input

--- internal/route/source.go
+++ internal/route/source.go
@@ -45,50 +45,52 @@ func (*Source) Report(ctx context.Context) {
 
 	switch req.Type {
 	case model.ReportTypeUpdateMember:
-		var updateMember model.UpdateMember
-		if err := jsoniter.Unmarshal(req.Data, &updateMember); err != nil {
+		var updateMembers []model.UpdateMember
+		if err := jsoniter.Unmarshal(req.Data, &updateMembers); err != nil {
 			ctx.Error(http.StatusBadRequest, err)
 			return
 		}
 
-		if err := db.Members.Create(ctx.Request().Context(), db.CreateMemberOptions{
-			SecUID:    updateMember.SecUID,
-			UID:       updateMember.UID,
-			UniqueID:  updateMember.UniqueID,
-			ShortUID:  updateMember.ShortUID,
-			Name:      updateMember.Name,
-			AvatarURL: updateMember.AvatarURL,
-			Signature: updateMember.Signature,
-		}); err != nil {
-			log.Error("Failed to create new member: %v", err)
-			ctx.ServerError()
-			return
+		for _, updateMember := range updateMembers {
+			if err := db.Members.Create(ctx.Request().Context(), db.CreateMemberOptions{
+				SecUID:    updateMember.SecUID,
+				UID:       updateMember.UID,
+				UniqueID:  updateMember.UniqueID,
+				ShortUID:  updateMember.ShortUID,
+				Name:      updateMember.Name,
+				AvatarURL: updateMember.AvatarURL,
+				Signature: updateMember.Signature,
+			}); err != nil {
+				log.Error("Failed to create new member: %v", err)
+				continue
+			}
 		}
 
 	case model.ReportTypeCreateVideo:
-		var createVideo model.CreateVideo
-		if err := jsoniter.Unmarshal(req.Data, &createVideo); err != nil {
+		var createVideos []model.CreateVideo
+		if err := jsoniter.Unmarshal(req.Data, &createVideos); err != nil {
 			ctx.Error(http.StatusBadRequest, err)
 			return
 		}
 
-		if err := db.Videos.Create(ctx.Request().Context(), createVideo.ID, db.CreateVideoOptions{
-			AuthorSecUID:     createVideo.AuthorSecUID,
-			Description:      createVideo.Description,
-			TextExtra:        createVideo.TextExtra,
-			OriginCoverURLs:  createVideo.OriginCoverURLs,
-			DynamicCoverURLs: createVideo.DynamicCoverURLs,
-			VideoHeight:      createVideo.VideoHeight,
-			VideoWidth:       createVideo.VideoWidth,
-			VideoDuration:    createVideo.VideoDuration,
-			VideoRatio:       createVideo.VideoRatio,
-			VideoURLs:        createVideo.VideoURLs,
-			VideoCDNURL:      createVideo.VideoCDNURL,
-			CreatedAt:        createVideo.CreatedAt,
-		}); err != nil && err != db.ErrVideoExists {
-			log.Error("Failed to create new video: %v", err)
-			ctx.ServerError()
-			return
+		for _, createVideo := range createVideos {
+			if err := db.Videos.Create(ctx.Request().Context(), createVideo.ID, db.CreateVideoOptions{
+				AuthorSecUID:     createVideo.AuthorSecUID,
+				Description:      createVideo.Description,
+				TextExtra:        createVideo.TextExtra,
+				OriginCoverURLs:  createVideo.OriginCoverURLs,
+				DynamicCoverURLs: createVideo.DynamicCoverURLs,
+				VideoHeight:      createVideo.VideoHeight,
+				VideoWidth:       createVideo.VideoWidth,
+				VideoDuration:    createVideo.VideoDuration,
+				VideoRatio:       createVideo.VideoRatio,
+				VideoURLs:        createVideo.VideoURLs,
+				VideoCDNURL:      createVideo.VideoCDNURL,
+				CreatedAt:        createVideo.CreatedAt,
+			}); err != nil && err != db.ErrVideoExists {
+				log.Error("Failed to create new video: %v", err)
+				continue
+			}
 		}
 
 	default:
