cd98eb1373c0ac86ddf6a21571da997f45976729
E99p1ant <i@github.red>
2021-09-25 16:40:44 +0800

    fix(db): drop videos_url column

--- migrations/2_create_url_videos_table.down.sql
+++ migrations/2_create_url_videos_table.down.sql
@@ -2,4 +2,10 @@ BEGIN;
 
 DROP TABLE IF EXISTS video_urls;
 
+ALTER TABLE videos
+    ADD COLUMN video_urls TEXT[] NOT NULL;
+
+ALTER TABLE videos
+    ADD COLUMN video_cdn_url TEXT NOT NULL;
+
 COMMIT;
--- migrations/2_create_url_videos_table.up.sql
+++ migrations/2_create_url_videos_table.up.sql
@@ -1,5 +1,10 @@
 BEGIN;
 
+ALTER TABLE videos
+    DROP COLUMN IF EXISTS video_urls,
+    DROP COLUMN IF EXISTS video_cdn_url;
+
+
 CREATE TABLE IF NOT EXISTS video_urls (
     video_id      TEXT                     NOT NULL,
     url           TEXT                     NOT NULL,
@@ -10,8 +15,8 @@ CREATE TABLE IF NOT EXISTS video_urls (
 ALTER TABLE ONLY video_urls
     ADD CONSTRAINT video_urls_pkey PRIMARY KEY ( url );
 
-CREATE INDEX video_urls_status ON video_urls( status );
+CREATE INDEX IF NOT EXISTS video_urls_status ON video_urls( status );
 
-CREATE INDEX video_urls_video_id ON video_urls( video_id, status );
+CREATE INDEX IF NOT EXISTS video_urls_video_id ON video_urls( video_id, status );
 
 COMMIT;
