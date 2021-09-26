c1dc02b8ecc48b8919b43fdc971dcf6fb89a2654
E99p1ant <i@github.red>
2021-09-27 03:46:36 +0800

    feat(fe): link to GitHub

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,6 +8,15 @@
       <div class="d-flex align-center">
         ASOUL-Video
       </div>
+      <v-spacer></v-spacer>
+      <v-btn
+          href="https://github.com/asoul-video"
+          target="_blank"
+          text
+      >
+        <span class="mr-2">GitHub</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
     </v-app-bar>
     <v-main>
       <router-view/>
