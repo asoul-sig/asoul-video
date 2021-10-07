f154d0eaec20ab5010d12b9436406db3a20f0fb2
E99p1ant <i@github.red>
2021-10-07 23:46:36 +0800

    feat(fe): friend link

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,6 +8,27 @@
         A-SOUL Video
       </div>
       <v-spacer></v-spacer>
+      <v-menu offset-y open-on-hover>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+              dark
+              text
+              v-bind="attrs"
+              v-on="on"
+          >
+            友链
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item-group>
+            <v-list-item v-for="(link, index) in friendLink" v-bind:key="index">
+              <v-list-item-action @click="open(link.url)">
+                {{ link.name }}
+              </v-list-item-action>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-menu>
       <v-btn
           href="https://github.com/asoul-video"
           target="_blank"
@@ -28,5 +49,17 @@
 <script>
 export default {
   name: 'App',
+
+  data() {
+    return {
+      friendLink: [{"name": "枝网查重", "url": "https://asoulcnki.asia/"}]
+    }
+  },
+
+  methods: {
+    open(url) {
+      window.open(url, '_blank')
+    }
+  }
 };
 </script>
