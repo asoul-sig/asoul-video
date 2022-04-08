11048f5375b129eef7e075310635ca39e606b220
E99p1ant <i@github.red>
2022-04-08 22:53:25 +0800

    fe: use local fonts

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
+import 'typeface-roboto/index.css';
+import '@mdi/font/css/materialdesignicons.css'
 
 Vue.config.productionTip = false
 
